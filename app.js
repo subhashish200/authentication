bf4b7d616c7a290d3192871ac4f616d75239f80e
Subhashish <subhashishde08@gmail.com>
2023-04-08 09:32:12 +0530

    All chapter of Authentication section in udemy  are completed

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -109,14 +110,40 @@ app.get("/register", function(req,res){
 });
 
 app.get("/secrets",function(req,res){
+    User.find({"secret": {$ne: null}}) // secret field is not equal to null
+      .then(function (foundUsers) {
+        if(foundUsers){
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+      })
+      .catch(function (err) {
+       console.log(err);
+      });
+});
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
         res.redirect("/login");
     }
 });
+app.post("/submit" , function(req,res){
+    const submittedSecret = req.body.secret;
+    console.log(req.user.id);
 
+    User.findById(req.user.id)
+    .then(function (foundUser) {
+      if(foundUser){
+          foundUser.secret = submittedSecret;
+          foundUser.save();
+          res.redirect("/secrets");
+      }
+    })
+    .catch(function (err) {
+       console.log(err);
+    });
+});
 app.get("/logout", function(req, res, next){
     req.logout(function(err) {
       if (err) { return next(err); }
